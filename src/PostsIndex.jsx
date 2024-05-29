0dde2d8a8c446b248347a270c38a54dcba574ff7
Arroneous2 <thumpinbassbeat@gmail.com>
2024-05-29 09:47:07 -0500

    fix child issues

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -1,11 +1,10 @@
 export function PostsIndex(props) {
-  console.log("The props are", props);
   return (
     <div id="posts-index">
       <h1>All posts</h1>
       <p>The name is</p>
       {props.posts.map((item) => (
-        <div className="posts-index ">
+        <div key={item.id} className="posts-index ">
           <h2>{item.title}</h2>
           <p>{item.body}</p>
           <img src={item.image} alt="" />
