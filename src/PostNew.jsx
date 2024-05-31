14eaf80c06c50bd5d672112b08b89e2bb6d8befa
Arroneous2 <thumpinbassbeat@gmail.com>
2024-05-31 11:43:10 -0500

    Simple post new from form

--- src/PostNew.jsx
+++ src/PostNew.jsx
@@ -7,10 +7,25 @@ export function PostNew() {
         quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean
         ultricies mi vitae est. Mauris placerat eleifend leo.
       </p>
-      <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnkLob1mWBzomOMpCn29Fz4XgykktGN-NuQ6NV8XyH1w&s"
-        alt="Some lorem Ipsum"
-      />
+      <form method="POST" action="http://localhost:3000/posts.json">
+        <div>
+          <label htmlFor="user_id">User ID:</label>
+          <input type="text" name="user_id" />
+        </div>
+        <div>
+          <label htmlFor="title">Title:</label>
+          <input type="text" name="title" />
+        </div>
+        <div>
+          <label htmlFor="body">Body:</label>
+          <input type="text" name="body" />
+        </div>
+        <div>
+          <label htmlFor="image">Image:</label>
+          <input type="text" name="image" />
+        </div>
+        <button type="submit">create post</button>
+      </form>
     </div>
   );
 }
